afd826e15519288bb73863576fd004c4239e32d4
nowgnas <marshmello.sw@gmail.com>
2022-12-13 16:52:25 +0900

    spring 입문 코드 추가

---
+++ 상원/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,36 @@
+package hello.hellospring;
+
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new JDBCMemberRepository(dataSource);
+////        return new JPAMemberRepository(em);
+//
+//    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+}
